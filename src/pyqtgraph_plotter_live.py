17e25331571b9bbabc20e38b2486df53a53807ce
Reza Qorbani <32905154+rezaqorbani@users.noreply.github.com>
2023-02-26 17:33:36 +0100

    seperate the classes for window and audio processing

--- src/pyqtgraph_plotter_live.py
+++ src/pyqtgraph_plotter_live.py
@@ -14,33 +14,21 @@ import pyqtgraph as pg
 import sys
 
 
-class LiveAudioPlot(QtWidgets.QMainWindow):
-    def __init__(self, channels, *args, **kwargs):
-        super(LiveAudioPlot, self).__init__(*args, **kwargs)
+class LiveAudio():
+    def __init__(self, nchannels, curves):
+        self.nchannels = nchannels
+        self.curves = curves
+
         # Set up audio stream
         self.audio = pyaudio.PyAudio()
         self.chunk_size = 1024
         self.format = pyaudio.paFloat32
-        self.channels = channels
+        self.channels = nchannels
         self.rate = 44100
         self.stream = self.audio.open(format=self.format, channels=self.channels,
                                       rate=self.rate, input=True,
                                       frames_per_buffer=self.chunk_size)
-
-        # Set up plot
-        self.win = pg.GraphicsLayoutWidget(title='Live Audio Plot')
-        self.win.resize(800, 600)
-        self.win.setWindowTitle('Live Audio Plot')
-        self.plots = []
-        self.curves = []	
-        for i in range(self.channels):
-            
-            self.plots.append(self.win.addPlot(title='Channel {}'.format(i)))
-
-            self.plots[i].setYRange(-1, 1)
-
-            self.curves.append(self.plots[i].plot(pen='y'))
-
+        # Set up x and y arrays
         self.x = np.arange(0, 2 * self.chunk_size, 2)
         self.data = np.zeros(self.chunk_size)
 
@@ -60,20 +48,40 @@ class LiveAudioPlot(QtWidgets.QMainWindow):
 
         # Update plot
         time_array = np.arange(len(self.data)) / float(self.rate)
+        
         for i in range(self.channels):
             self.curves[i].setData(time_array[-self.plot_length * self.rate:], self.data[-self.plot_length * self.rate:])
-       # self.curve.setData(time_array[-self.plot_length * self.rate:], self.data[-self.plot_length * self.rate:])
-
 
     def close(self):
         self.stream.stop_stream()
         self.stream.close()
         self.audio.terminate()
 
+class MainWindow(QtWidgets.QMainWindow):
+    def __init__(self, nchannels, *args, **kwargs):
+        super(MainWindow, self).__init__( *args, **kwargs)
+        self.nchannels = nchannels
+         # Set up plot
+        self.win = pg.GraphicsLayoutWidget( title='Live Audio Plot' )
+        self.setCentralWidget(self.win)
+        self.win.resize(800, 600)
+        self.win.setWindowTitle('Live Audio Plot')
+        self.plots = []
+        self.curves = []	
+        for i in range(self.nchannels):
+            
+            self.plots.append(self.win.addPlot(title='Channel {}'.format(i)))
+
+            self.plots[i].setYRange(-1, 1)
+
+            self.curves.append(self.plots[i].plot(pen='y'))
+
+        self.lap = LiveAudio( nchannels=self.nchannels, curves=self.curves )
+
 
 if __name__ == '__main__':
 
     app = QtWidgets.QApplication([])
-    lap = LiveAudioPlot(channels=3)
-    lap.show()
+    win = MainWindow(nchannels=2)
+    win.show()
     sys.exit(app.exec())
