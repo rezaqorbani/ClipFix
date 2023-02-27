a9b1dd81b0791de5e8969d71fb5bea137273d5ab
khalidey <102597887+khalidey@users.noreply.github.com>
2023-02-28 00:25:38 +0100

    Update pyqtgraph_plotter_live.py

--- src/pyqtgraph_plotter_live.py
+++ src/pyqtgraph_plotter_live.py
@@ -8,44 +8,33 @@ close() function stops the audio stream and terminates the PyAudio instance when
 
 import pyaudio
 import numpy as np
+from PyQt6.QtWidgets import QMessageBox
 from PyQt6 import QtCore, QtWidgets
-from pyqtgraph import PlotWidget, plot
+from PyQt6.QtGui import QFont
 import pyqtgraph as pg
 import sys
 
 
-class LiveAudioPlot(QtWidgets.QMainWindow):
-    def __init__(self, channels, *args, **kwargs):
-        super(LiveAudioPlot, self).__init__(*args, **kwargs)
+class LiveAudio():
+    def __init__(self, nchannels, curves):
+
+        self.curves = curves
+        self.recording = False
+
         # Set up audio stream
         self.audio = pyaudio.PyAudio()
         self.chunk_size = 1024
         self.format = pyaudio.paFloat32
-        self.channels = channels
+        self.nchannels = nchannels
         self.rate = 44100
-        self.stream = self.audio.open(format=self.format, channels=self.channels,
-                                      rate=self.rate, input=True,
-                                      frames_per_buffer=self.chunk_size)
+        self.stream: pyaudio.Stream = None
 
-        # Set up plot
-        self.win = pg.GraphicsLayoutWidget(show=True, title='Live Audio Plot')
-        self.win.resize(800, 600)
-        self.win.setWindowTitle('Live Audio Plot')
-        self.plots = []
-        self.curves = []	
-        for i in range(self.channels):
-            
-            self.plots.append(self.win.addPlot(title='Channel {}'.format(i)))
-
-            self.plots[i].setYRange(-1, 1)
-
-            self.curves.append(self.plots[i].plot(pen='y'))
-
+        # Set up x and y arrays
         self.x = np.arange(0, 2 * self.chunk_size, 2)
         self.data = np.zeros(self.chunk_size)
 
         # Set up plot length
-        self.plot_length = 10  # set the plot length to 5 seconds
+        self.plot_length = 5  # set the plot length to 5 seconds
 
         # Set up timer
         self.timer = QtCore.QTimer()
@@ -54,25 +43,122 @@ class LiveAudioPlot(QtWidgets.QMainWindow):
 
     def update(self):
         # Get audio data
-        raw_data = self.stream.read(self.chunk_size)
-        data = np.frombuffer(raw_data, dtype=np.float32)
-        self.data = np.concatenate((self.data, data))
+        if self.recording:
+            raw_data = self.stream.read(self.chunk_size)
+            data = np.frombuffer(raw_data, dtype=np.float32)
+            self.data = np.concatenate((self.data, data))
 
-        # Update plot
-        time_array = np.arange(len(self.data)) / float(self.rate)
-        for i in range(self.channels):
-            self.curves[i].setData(time_array[-self.plot_length * self.rate:], self.data[-self.plot_length * self.rate:])
-       # self.curve.setData(time_array[-self.plot_length * self.rate:], self.data[-self.plot_length * self.rate:])
+            # Update plot
+            time_array = np.arange(len(self.data)) / float(self.rate)
 
+            for i in range(self.nchannels):
+                self.curves[i].setData(time_array[-self.plot_length * self.rate:],
+                                       self.data[-self.plot_length * self.rate:])
 
-    def close(self):
+    def startRecording(self):
+        self.recording = True
+        self.stream = self.audio.open(format=self.format, channels=self.nchannels,
+                                      rate=self.rate, input=True,
+                                      frames_per_buffer=self.chunk_size)
+
+    def stopRecording(self):
+        self.recording = False
+        self.stream.stop_stream()
+
+    def closeSession(self):
+        self.recording = False
         self.stream.stop_stream()
         self.stream.close()
         self.audio.terminate()
 
+    def isReocrding(self):
+        return self.recording
+
+
+class MainWindow(QtWidgets.QMainWindow):
+    def __init__(self, nchannels, *args, **kwargs):
+        super(MainWindow, self).__init__(*args, **kwargs)
+
+        self.nchannels = nchannels
+
+        # Set up plot
+        self.win = pg.GraphicsLayoutWidget(title='Live Audio Plot')
+        self.setCentralWidget(self.win)
+        self.win.resize(800, 600)
+        self.win.setWindowTitle('Live Audio Plot')
+        self.inputPlots = []
+        self.curves = []
+        for i in range(self.nchannels):
+            self.inputPlots.append(self.win.addPlot(row=0, col=i, title='Channel {}'.format(i+1)))
+            self.inputPlots[i].setYRange(-1, 1)
+            self.inputPlots[i].setLabels(left='Amplitude', bottom='Time (s)')
+            self.inputPlots[i].getAxis('bottom').setStyle(tickFont=QFont('Arial', 10), autoExpandTextSpace=True)
+            self.inputPlots[i].getAxis('left').setStyle(tickFont=QFont('Arial', 10), autoExpandTextSpace=True)
+            self.inputPlots[i].showGrid(x=True, y=True, alpha=0.5)
+            self.curves.append(self.inputPlots[i].plot(pen=pg.mkPen('y', width=2)))
+
+        self.outputPlot = self.win.addPlot(row=1, col=0, title='Output')
+        self.outputPlot.setLabels(left='Amplitude', bottom='Time (s)')
+        self.outputPlot.getAxis('bottom').setStyle(tickFont=QFont('Arial', 10), autoExpandTextSpace=True)
+        self.outputPlot.getAxis('left').setStyle(tickFont=QFont('Arial', 10), autoExpandTextSpace=True)
+        self.outputPlot.showGrid(x=True, y=True, alpha=0.5)
+
+        # Set up live audio
+        self.liveAudio = LiveAudio(nchannels=self.nchannels, curves=self.curves)
+
+        if self.nchannels == 1:
+            self.recordButtonProxy = QtWidgets.QGraphicsProxyWidget()
+            self.recordButton = QtWidgets.QPushButton('Record')
+            self.recordButton.clicked.connect(self.record)
+            self.recordButtonProxy.setWidget(self.recordButton)
+            self.win.addItem(self.recordButtonProxy, row=2, col=self.nchannels)
+
+            self.stopButtonProxy = QtWidgets.QGraphicsProxyWidget()
+            self.stopButton = QtWidgets.QPushButton('Stop')
+            self.stopButton.clicked.connect(self.stop)
+            self.stopButtonProxy.setWidget(self.stopButton)
+            self.win.addItem(self.stopButtonProxy, row=2, col=self.nchannels+1)
+
+        else:
+            self.recordButtonProxy = QtWidgets.QGraphicsProxyWidget()
+            self.recordButton = QtWidgets.QPushButton('Record')
+            self.recordButton.clicked.connect(self.record)
+            self.recordButtonProxy.setWidget(self.recordButton)
+            self.win.addItem(self.recordButtonProxy, row=2, col=self.nchannels-2)
+
+            self.stopButtonProxy = QtWidgets.QGraphicsProxyWidget()
+            self.stopButton = QtWidgets.QPushButton('Stop')
+            self.stopButton.clicked.connect(self.stop)
+            self.stopButtonProxy.setWidget(self.stopButton)
+            self.win.addItem(self.stopButtonProxy, row=2, col=self.nchannels-1)
+
+    def record(self):
+        self.liveAudio.startRecording()
+
+    def stop(self):
+        self.liveAudio.stopRecording()
+
+    def closeEvent(self, event):
+        if self.liveAudio.isReocrding():
+            # Ask the user if they really want to exit
+            reply = QMessageBox.question(self, 'Confirm Exit', 'Are you sure you want to exit?',
+                                         QMessageBox.StandardButton.Yes | QMessageBox.StandardButton.No,
+                                         QMessageBox.StandardButton.No)
+            if reply == QMessageBox.StandardButton.Yes:
+                # Perform cleanup here
+                print('Exiting...')
+                self.liveAudio.closeSession()
+                event.accept()
+            else:
+                event.ignore()
+        else:
+            # Perform cleanup here
+            print('Cleaning up before exit...')
+            event.accept()
+
 
 if __name__ == '__main__':
-
     app = QtWidgets.QApplication([])
-    lap = LiveAudioPlot(channels=3)
+    win = MainWindow(nchannels=3)
+    win.show()
     sys.exit(app.exec())
