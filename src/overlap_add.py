bcf68ef9802ca4c498fc1294f18a9c9cab85c632
Zineb Senane <senane.zineb.mpsi@gmail.com>
2023-03-02 22:55:52 +0100

    Update overlap_add implementation

--- src/overlap_add.py
+++ src/overlap_add.py
@@ -1,101 +1,131 @@
 # Import Packages
 import wave
 import numpy as np
-from scipy import signal as sg
+from scipy import signal
+from sequence_generation2 import gen
+import matplotlib.pyplot as plt
 
+# Get signals at different gain levels using the script
+signals=gen(plot_flag = False)
 
 def mix(signals):
     """_summary_
 
     Args:
-        signals (List of String elements): list of the audio files names to be mixed
+        signals (Numpy Array): Multi-channel signal at different gain levels
         
     Output:
         The new synthetized signal by mixing the input signals using overlap_add method
     """
-    audio_arrays = []
+    audio_arrays = signals[:,0:5:4]
     audio_params = []
     
-    # Read the two input audio files
-    for signal in signals:
-        with wave.open(signal,'rb') as audio_file:
-            # Get the audio file parameters
-            params = audio_file.getparams()
-            # Read all audio frames into a byte string
-            audio_frames = audio_file.readframes(params.nframes)
-            # Convert the byte string to a NumPy array
-            audio_array = np.frombuffer(audio_frames, dtype=np.int16)/32768.0 ##
-            audio_params.append(params)
-            audio_arrays.append(audio_array)
-
+    
     # Ensure that both audio signals have the same number of channels
 
 
     # Ensure that both audio signals have the same sample rate
 
+    # Define signal params
+    signal_duration = 60
+    sr = 16000
+    harm_freq = None
 
+    # Quantization parameters
+    n_bits_in = 24
+    n_bits_q = 16
+    
     # Define the window size and overlap
-    window_size = 1024
-    overlap = 650 #window_size // 2
+    frame_len = int(0.03 * sr)
+    frame_hop = int(0.015 * sr)
 
-    # Initialize the overlap-add buffers
-    overlap_add_buffer1 = np.zeros((overlap))
-    overlap_add_buffer2 = np.zeros((overlap))
+    # Initialize the overlap-add buffer
+    overlap_add_buffer = np.zeros((frame_hop))
+    
     
     # Process the audio signals in chunks
-    output = np.zeros((len(audio_arrays[0])))
-    for i in range(0, len(audio_arrays[0]) - window_size, overlap):
+    output = np.zeros((audio_arrays.shape[0]))
+    
+    for i in range(0, audio_arrays.shape[0] - frame_len, frame_hop):
         # Extract a chunk of audio data from each signal
-        chunk1 = audio_arrays[0][i:i+window_size]
-        chunk2 = audio_arrays[1][i:i+window_size]
+        chunk1 = audio_arrays[i:i+frame_len,0]
+        chunk2 = audio_arrays[i:i+frame_len,1]
+        
+        # Detect the clipping and keep the chunk with high energy
+        is_clipped_1=detect_clipping(chunk1)[0]
+        is_clipped_2=detect_clipping(chunk2)[0]
+        
+        if is_clipped_1:
+            frame_to_keep=chunk2
+        elif is_clipped_2:
+            frame_to_keep=chunk1
+        else:
+            # Calculate the energy of each channel in each chunk
+            energies1 = np.sqrt(np.mean(np.square(chunk1), axis=0))
+            energies2 = np.sqrt(np.mean(np.square(chunk2), axis=0))
+            print(energies1,energies2)
+            
+
+            # Select the chunk with the higher energy for each channel
+            mask1 = energies1 > energies2
+            mask2 = ~mask1
+            vars_arr=np.array([mask1,mask2])
+            coefficients = np.where(vars_arr == 1, 0.6, 0.4 )
+            # Mix the chunks using the selected gains
+            frame_to_keep = coefficients[0] * chunk1 + coefficients[1] * chunk2
+
+        
         # Apply a window function to the data
-        window = np.hanning(window_size)
-        chunk1 *= window
-        chunk2 *= window
+        window = signal.hann(frame_len)
+        frame_to_keep = (frame_to_keep*window)     
 
         # Add the overlap buffers to the start of the data
-        chunk1[:overlap] += overlap_add_buffer1
-        chunk2[:overlap] += overlap_add_buffer2
-
-        # Calculate the energy of each channel in each chunk
-        energies1 = np.sqrt(np.mean(np.square(chunk1), axis=0))
-        energies2 = np.sqrt(np.mean(np.square(chunk2), axis=0))
-        print(energies1,energies2)
+        frame_to_keep[:frame_hop] += overlap_add_buffer
         
-
-        # Select the chunk with the higher energy for each channel
-        mask1 = energies1 > energies2
-        mask2 = ~mask1
-        vars_arr=np.array([mask1,mask2])
-        coefficients = np.where(vars_arr == 1, 0.6, 0.4 )
-        # Mix the chunks using the selected gains
-        mixed_chunk = coefficients[0] * chunk1 + coefficients[1] * chunk2
-
         # Reconstruct the audio signal by summing the mixed chunks
-        output[i:i+window_size] += mixed_chunk
+        output[i:i+frame_len] += frame_to_keep
 
         # Update the overlap-add buffers
-        overlap_add_buffer1 = chunk1[-overlap:]
-        overlap_add_buffer2 = chunk2[-overlap:]
+        overlap_add_buffer = frame_to_keep[-frame_hop:]
+
+    plt.figure()
+    plt.plot(output)
+    plt.show()
+    print('The generated signal is clpped: ', detect_clipping(output)[0])
+    return output
+
+
         
-   
-        # Define the filter parameters
-        cutoff_freq = 7000  # Hz
-        nyquist_freq = 0.5 * 44100  # Nyquist frequency
-        cutoff_norm = cutoff_freq / nyquist_freq
-        order = 6  # Filter order
+def detect_clipping(signal_chunk):
+    nmax = max(signal_chunk)
+    nmin = min(signal_chunk)
+    clipped_segments = []
+    inside_clip = False
+    clip_start = 0
+    clip_end = 0
+    
+    for i, sample in enumerate(signal_chunk):
+        #sample equal to or extremely close to max or min
+        if (sample <= nmin+1) or (sample >= nmax-1):
+            if not inside_clip:
+                # declare we are inside clipped segment
+                inside_clip = True
+                # this is the first clipped sample
+                clip_start = i
+        elif inside_clip:
+            inside_clip = False # no longer inside clipped segment
+            clip_end = i-1  # previous sample is end of segment
+            # save segment as tuple
+            clipped_segment = [clip_start, clip_end]
+            # store tuple in list of clipped segments
+            clipped_segments.append(clipped_segment)
+    clipped_segments=np.array(clipped_segments)
+    is_clipped=np.any(np.diff(clipped_segments,axis=1)!=0)
+    
+    return is_clipped, clipped_segments
 
-        # Create the filter coefficients
-        b, a = sg.butter(order, cutoff_norm, btype='lowpass')
 
-        # Apply the filter to the audio signal
-        filtered_audio = sg.filtfilt(b, a, output*32767)
-
-
-
-    # Save mixed signal to file
-    with wave.open('mixed_wighted_avg_signal.wav', 'wb') as wav:
-        wav.setparams(audio_params[0])
-        wav.writeframes((filtered_audio+10).astype(np.int16).tobytes())#output * 32767
+detect_clipping(signals[:,7])
+mix(signals)
         
 
