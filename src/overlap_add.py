e75178ac04968c96b77f257de2fcda5c968a058f
Zineb Senane <senane.zineb.mpsi@gmail.com>
2023-03-06 11:25:52 +0100

    adjust clipping detection

--- src/overlap_add.py
+++ src/overlap_add.py
@@ -1,5 +1,4 @@
 # Import Packages
-import wave
 import numpy as np
 from scipy import signal
 from sequence_generation2 import gen
@@ -7,6 +6,7 @@ import matplotlib.pyplot as plt
 
 # Get signals at different gain levels using the script
 signals=gen(plot_flag = False)
+two_channel_signal = signals[:,0:5:4]
 
 def mix(signals):
     """_summary_
@@ -17,23 +17,12 @@ def mix(signals):
     Output:
         The new synthetized signal by mixing the input signals using overlap_add method
     """
-    audio_arrays = signals[:,0:5:4]
-    audio_params = []
     
-    
-    # Ensure that both audio signals have the same number of channels
 
-
-    # Ensure that both audio signals have the same sample rate
-
-    # Define signal params
-    signal_duration = 60
+    # Define sampling rate
     sr = 16000
-    harm_freq = None
+    bit_depth= 16
 
-    # Quantization parameters
-    n_bits_in = 24
-    n_bits_q = 16
     
     # Define the window size and overlap
     frame_len = int(0.03 * sr)
@@ -44,22 +33,25 @@ def mix(signals):
     
     
     # Process the audio signals in chunks
-    output = np.zeros((audio_arrays.shape[0]))
+    output = np.zeros((signals.shape[0]))
     
-    for i in range(0, audio_arrays.shape[0] - frame_len, frame_hop):
+    for i in range(0, signals.shape[0] - frame_len, frame_hop):
         # Extract a chunk of audio data from each signal
-        chunk1 = audio_arrays[i:i+frame_len,0]
-        chunk2 = audio_arrays[i:i+frame_len,1]
+        chunk1 = signals[i:i+frame_len,0]
+        chunk2 = signals[i:i+frame_len,1]
         
         # Detect the clipping and keep the chunk with high energy
-        is_clipped_1=detect_clipping(chunk1)[0]
-        is_clipped_2=detect_clipping(chunk2)[0]
+        is_clipped_1=is_clipped(chunk1,bit_depth)
+        is_clipped_2=is_clipped(chunk2,bit_depth)
         
         if is_clipped_1:
             frame_to_keep=chunk2
         elif is_clipped_2:
             frame_to_keep=chunk1
         else:
+            # TODO: Detect quantisation noise
+            
+            
             # Calculate the energy of each channel in each chunk
             energies1 = np.sqrt(np.mean(np.square(chunk1), axis=0))
             energies2 = np.sqrt(np.mean(np.square(chunk2), axis=0))
@@ -91,20 +83,21 @@ def mix(signals):
     plt.figure()
     plt.plot(output)
     plt.show()
-    print('The generated signal is clpped: ', detect_clipping(output)[0])
+    
     return output
 
 
         
-def detect_clipping(signal_chunk):
-    nmax = max(signal_chunk)
-    nmin = min(signal_chunk)
+def detect_clipping(signal_chunk, bit_depth):
+    threshold = 2 ** (bit_depth - 1) - 1
     clipped_segments = []
     inside_clip = False
     clip_start = 0
     clip_end = 0
     
     for i, sample in enumerate(signal_chunk):
+        nmin=min(signal_chunk)
+        nmax=max(signal_chunk)
         #sample equal to or extremely close to max or min
         if (sample <= nmin+1) or (sample >= nmax-1):
             if not inside_clip:
@@ -124,8 +117,19 @@ def detect_clipping(signal_chunk):
     
     return is_clipped, clipped_segments
 
+def is_clipped(signal_chunk, bit_depth):
+    # Define clipping threshold value based on bit depth
+    threshold = 2 ** (bit_depth - 1) - 1
+    # Check if any value in the signal chunk exceeds the threshold
+    indices=np.argwhere(np.abs(signal_chunk.astype(np.int32)) >= threshold)
+    
+    return len(indices)!=0
 
-detect_clipping(signals[:,7])
-mix(signals)
+def quantization_noise(signal_chunk):
+    return None
+
+#print(is_clipped(signals[:,7],16))
+#detect_clipping(signals[:,7],16)
+mix(two_channel_signal)
         
 
